598932d53f37cac1d6242d999721e6c42782f14e
Olly Hayes <olly.hayes@gmail.com>
2015-10-15 18:49:23 +0100

    adding node-inspector

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if ! grep -q "source ~/tools/config/.bashrc" ~/.bashrc; then
 fi
 
 ln -s ~/tools/config/.gitconfig ~/.gitconfig
+ln -s ~/tools/config/.node-inspectorrc ~/.node-inspectorrc
 ln -s ~/tools/config/.inputrc ~/.inputrc
 ln -s ~/tools/config/.jshintrc ~/.jshintrc
 ln -s ~/tools/config/vimrc/_vimrc ~/.vimrc
