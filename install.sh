e78febc21202f05d81b75d65ea74b9a3aa0ac2c0
Olly Hayes <olly.hayes@gmail.com>
2016-01-14 18:00:56 +0000

    Added global install location for npm

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ln -s ~/tools/config/.gitconfig ~/.gitconfig
 ln -s ~/tools/config/.node-inspectorrc ~/.node-inspectorrc
 ln -s ~/tools/config/.inputrc ~/.inputrc
 ln -s ~/tools/config/.jshintrc ~/.jshintrc
+ln -s ~/tools/config/.npmrc ~/.npmrc
 ln -s ~/tools/config/vimrc/_vimrc ~/.vimrc
 ln -s ~/tools/config/vimrc/_gvimrc-high-res ~/.gvimrc
