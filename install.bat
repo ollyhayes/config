15452df722670317e1d0d2ffae997fd264d2d9fc
Olly Hayes <olly.hayes@gmail.com>
2016-08-13 07:34:20 +0100

    Add installer for windows

---
+++ install.bat
@@ -0,0 +1,8 @@
+mklink %USERPROFILE%\.bashrc %USERPROFILE%\tools\config\.bashrc
+mklink %USERPROFILE%\.gitconfig %USERPROFILE%\tools\config\.gitconfig
+mklink %USERPROFILE%\.node-inspectorrc %USERPROFILE%\tools\config\.node-inspectorrc 
+mklink %USERPROFILE%\.inputrc %USERPROFILE%\tools\config\.inputrc
+mklink %USERPROFILE%\.jshintrc %USERPROFILE%\tools\config\.jshintrc
+mklink %USERPROFILE%\.npmrc %USERPROFILE%\tools\config\.npmrc
+mklink %USERPROFILE%\.vimrc %USERPROFILE%\tools\config\vimrc\_vimrc
+mklink %USERPROFILE%\.gvimrc %USERPROFILE%\tools\config\vimrc\_gvimrc
