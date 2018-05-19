d51b13c68cf18094c56050c902d3ee83d8c1a088
Olly Hayes <olly.hayes@gmail.com>
2018-05-18 22:19:11 -0500

    Invoke directly

--- snippets/decrypt-passwords.sh
+++ snippets/decrypt-passwords.sh
@@ -3,6 +3,11 @@
 OUTPUT=~/storage/documents
 CPT_FILE=~/storage/documents/passwords.cpt
 
+read -sp "Enter decryption key: " PASSWORD
+echo
+
+export PASSWORD
+
 echo Decrypting...
 ccat --envvar PASSWORD $CPT_FILE | tar -xC $OUTPUT
 
@@ -11,7 +16,7 @@ read -p "Finished. Update data (y/N)?"
 if [ "$REPLY" == "y" ]
 then
 	echo Re-encrypting...
-	tar -c $OUTPUT/data | ccencrypt --envvar PASSWORD > $CPT_FILE
+	tar -cC $OUTPUT $OUTPUT/data | ccencrypt --envvar PASSWORD > $CPT_FILE
 fi
 
 rm $OUTPUT/data -R
