ad87dae5a76582ab93793417157a3d237d0585f9
Olly Hayes <olly.hayes@gmail.com>
2018-06-23 08:43:48 -0500

    Use user defined path

--- snippets/find-photos.sh
+++ snippets/find-photos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /tmp/photos/$1
+
 
 for file in $(find . -iname "*.jpg")
 do
@@ -8,6 +8,6 @@ do
 	if exiftool "$file" | grep "Rating *: 5"
 	then
 		echo "copying $file"
-		cp "$file" /tmp/photos/$1/
+		cp "$file" $1
 	fi
 done
