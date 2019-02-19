2e0798b1c49bd5d76b6c406916125b5defd7c7a1
Olly Hayes <olly.hayes@gmail.com>
2019-02-19 08:09:43 +0000

    Tmux helpers

--- snippets/bash-snippets.sh
+++ snippets/bash-snippets.sh
@@ -1,1 +1,8 @@
-git l
+function startTest()
+{
+	# to be run inside tmux
+	tmux new-window -c "[directory]" "[command for server]" # launch test server
+	tmux split-window -c "[directory]" # open a split
+	tmux resize-pane -t 0 -y 5 # set test server window small
+	tmux send-keys "[command to launch tests]" # get tests ready
+}
