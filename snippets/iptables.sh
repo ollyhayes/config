cd6147d52ad0aaca0d09dcd02f1c9b093cd2279e
Olly Hayes <olly.hayes@gmail.com>
2019-05-24 12:39:43 +0100

    Add iptables rules

--- snippets/iptables.sh
+++ snippets/iptables.sh
@@ -1,6 +1,8 @@
 # use netfilter-persistent
 # saved to /etc/iptables/rules.v4
 
+#iptable --table nat # use this to show the rules
+
 iptables -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8081
 iptables -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8082
 netfilter-persistent save
