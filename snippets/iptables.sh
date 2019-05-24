f55ba339ab200720703b425a713de400cb604290
Olly Hayes <olly.hayes@gmail.com>
2019-05-24 12:01:36 +0000

    Fix iptables snippet

--- snippets/iptables.sh
+++ snippets/iptables.sh
@@ -1,8 +1,12 @@
 # use netfilter-persistent
 # saved to /etc/iptables/rules.v4
 
-#iptable --table nat # use this to show the rules
+#iptable --list --table nat # use this to show the rules
 
-iptables -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8081
-iptables -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8082
-netfilter-persistent save
+iptables --table nat -A PREROUTING -p tcp -m tcp --dport 443 -j REDIRECT --to-ports 8081
+iptables --table nat -A PREROUTING -p tcp -m tcp --dport 80 -j REDIRECT --to-ports 8080
+apt install iptables-persistent
+
+# iptables-persistent saves when it installs
+# apparently if you want to do it after you can either edit the above file or use:
+# netfilter-persistent save
