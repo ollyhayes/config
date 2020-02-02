88cce8292f33231dafa92e63923a7e9a8b9fc9fe
Olly Hayes <olly.hayes@gmail.com>
2020-02-02 15:44:45 +0100

    Add more iptables notes

--- snippets/iptables.sh
+++ snippets/iptables.sh
@@ -10,3 +10,15 @@ apt install iptables-persistent
 # iptables-persistent saves when it installs
 # apparently if you want to do it after you can either edit the above file or use:
 # netfilter-persistent save
+
+# see keep for transmission notes
+
+# for restricting transmission to only going through the vpn
+
+#this one should open for the web interface but it does all of them
+#sudo iptables -A OUTPUT -d 192.168.0.0/24 -p tcp --sport 9091 -m owner --gid-owner debian-transmission -o enp1s0 -j ACCEPT
+sudo iptables -A OUTPUT -m owner --gid-owner debian-transmission \! -o tun0 -j REJECT
+
+#this currently doesn't work, transmission can still get out
+#next I'll try what's in this answer:
+#	https://askubuntu.com/a/462736
