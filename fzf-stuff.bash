ff5c53b6ec1b4b61a14d8a043a1934ec959c4055
Olly Hayes <olly.hayes@gmail.com>
2020-05-19 10:37:31 +0200

    Get remote branches also in ctrl-t

--- fzf-stuff.bash
+++ fzf-stuff.bash
@@ -108,7 +108,7 @@ bind -m vi-command '"\ec": "ddl`__fzf_cd__`\C-x\C-e\C-x\C-r\C-m"'
 
 function echo-fzf-selected-local-branch() {
   local branches branch result
-  branches=$(git bra) &&
+  branches=$(git bboth) &&
 
   result=$(echo "$branches" | fzf --ansi -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
   branch=$(echo "$result" | sed -E "s/^[ ->]{3}[0-9-]* ([^ ]*).*$/\1/")
