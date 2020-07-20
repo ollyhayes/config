295c93cf9c6de2fc735c517cb523415e0de09887
Olly Hayes <olly.hayes@gmail.com>
2020-07-20 20:22:48 +0200

    Update to use correct stcript for audio-switch

--- launch-programs.sh
+++ launch-programs.sh
@@ -1,4 +1,4 @@
 tmux new-session -ds launched-programs
 
 tmux new-window -n volume /home/olly/tools/volume-control/node_modules/.bin/electron /home/olly/tools/volume-control/index
-tmux new-window -n audio-switch python3 /home/olly/tools/audio-switch/index.py
+tmux new-window -n audio-switch python3 /home/olly/tools/snippets/audio-switch.py
