594b54e7a46c079c205733250e03bd22b0986ae1
Olly Hayes <olly.hayes@gmail.com>
2021-02-27 09:52:55 +0100

    Launch keyboard presentation if it's there

--- launch-programs.sh
+++ launch-programs.sh
@@ -4,3 +4,8 @@ tmux new-session -ds launched-programs
 
 tmux new-window -n volume /home/olly/tools/volume-control/node_modules/.bin/electron /home/olly/tools/volume-control/index
 tmux new-window -n audio-switch python3 /home/olly/tools/config/snippets/audio-switch.py
+
+if [ -f "/home/olly/tools/keyboard-presentation-prod/tmux.sh" ]
+then
+	~/tools/keyboard-presentation-prod/tmux.sh
+fi
