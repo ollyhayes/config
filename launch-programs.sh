63fc91a3288edfda44db42492ec7a0ef3b29a86a
Olly Hayes <olly.hayes@gmail.com>
2021-03-20 12:49:25 +0100

    Use correct directory for keyboard presentation

--- launch-programs.sh
+++ launch-programs.sh
@@ -5,7 +5,7 @@ tmux new-session -ds launched-programs
 tmux new-window -n volume /home/olly/tools/volume-control/node_modules/.bin/electron /home/olly/tools/volume-control/index
 tmux new-window -n audio-switch python3 /home/olly/tools/config/snippets/audio-switch.py
 
-if [ -f "/home/olly/tools/keyboard-presentation-prod/tmux.sh" ]
+if [ -f "/home/olly/tools/keyboard-presentation/tmux.sh" ]
 then
-	~/tools/keyboard-presentation-prod/tmux.sh
+	~/tools/keyboard-presentation/tmux.sh
 fi
