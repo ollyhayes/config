943177f47c00a20d00f8ebdcf8949dea334657d9
Olly Hayes <olly.hayes@gmail.com>
2021-06-13 13:03:47 +0200

    Add chrome installer

---
+++ install-userchrome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for PROFILE_DIR in ~/.mozilla/firefox/*
+do
+	if [ -d "$PROFILE_DIR" ] && [[ $(basename "$PROFILE_DIR") =~ ^[a-z0-9]{8}\. ]] # e.g o2tfcnw7.default-release
+	then
+		mkdir -p "$PROFILE_DIR/chrome"
+		ln -s ~/tools/config/userChrome.css $PROFILE_DIR/chrome
+	fi
+done
