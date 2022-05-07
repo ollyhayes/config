48785051d2c4746d80dec61fa40aa68cc439bad3
Olly Hayes <olly.hayes@gmail.com>
2022-05-07 09:14:41 +0200

    Force install user chrome

--- install-userchrome.sh
+++ install-userchrome.sh
@@ -5,6 +5,6 @@ do
 	if [ -d "$PROFILE_DIR" ] && [[ $(basename "$PROFILE_DIR") =~ ^[a-z0-9]{8}\. ]] # e.g o2tfcnw7.default-release
 	then
 		mkdir -p "$PROFILE_DIR/chrome"
-		ln -s ~/tools/config/userChrome.css $PROFILE_DIR/chrome
+		ln -fs ~/tools/config/userChrome.css $PROFILE_DIR/chrome
 	fi
 done
