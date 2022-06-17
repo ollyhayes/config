47101ef52165a096f8d6ebc7e60cda2d4481585c
Olly Hayes <olly.hayes@gmail.com>
2022-06-17 08:21:15 +0200

    Fix snap firefox chrome again

--- install-userchrome.sh
+++ install-userchrome.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for PROFILE_DIR in ~/.mozilla/firefox/* ~/snap/firefox/common/.cache/firefox/*
+for PROFILE_DIR in ~/.mozilla/firefox/* ~/snap/firefox/common/.mozilla/firefox/*
 do
 	if [ -d "$PROFILE_DIR" ] && [[ $(basename "$PROFILE_DIR") =~ ^[a-z0-9]{8}\. ]] # e.g o2tfcnw7.default-release
 	then
 		mkdir -p "$PROFILE_DIR/chrome"
 		ln -fs ~/tools/config/userChrome.css $PROFILE_DIR/chrome
+		echo Installed userChrome in $PROFILE_DIR/chrome
 	fi
 done
 
